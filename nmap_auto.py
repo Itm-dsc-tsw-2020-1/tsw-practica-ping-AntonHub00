c796cf512abda90e8e852c021dc4bb0ebe215374
Antonio Ochoa <antonhub00@gmail.com>
2020-02-27 12:32:20 -0600

    Practice with nmap aded

---
+++ nmap_auto.py
@@ -0,0 +1,32 @@
+import os
+
+os.system('nmap -sP 200.33.171.0/24')
+
+# Starting Nmap 7.60 ( https://nmap.org ) at 2020-02-27 12:22 CST
+# Nmap scan report for mail.itmorelia.edu.mx (200.33.171.3)
+# Host is up (0.0011s latency).
+# Nmap scan report for 200.33.171.13
+# Host is up (0.0010s latency).
+# Nmap scan report for sagitario.itmorelia.edu.mx (200.33.171.65)
+# Host is up (0.00086s latency).
+# Nmap scan report for 200.33.171.66
+# Host is up (0.00079s latency).
+# Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+# Host is up (0.0012s latency).
+# Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+# Host is up (0.0017s latency).
+# Nmap scan report for 200.33.171.85
+# Host is up (0.00085s latency).
+# Nmap scan report for 200.33.171.86
+# Host is up (0.0011s latency).
+# Nmap scan report for 200.33.171.99
+# Host is up (0.0015s latency).
+# Nmap scan report for 200.33.171.115
+# Host is up (0.013s latency).
+# Nmap scan report for 200.33.171.125
+# Host is up (0.0011s latency).
+# Nmap scan report for 200.33.171.127
+# Host is up (0.00094s latency).
+# Nmap done: 256 IP addresses (12 hosts up) scanned in 5.03 seconds
+
+# 12 hosts encontrados
